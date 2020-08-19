b5f46a54d6a991e55eaff75366732e84adc294e7
Anitaliky <anitaliky@gmail.com>
2020-08-19 19:36:53 +0300

    FNN - preprocess relevant features

--- FNN/Preprocces.py
+++ FNN/Preprocces.py
@@ -5,20 +5,12 @@ import torch.utils.data as data
 from torch import optim
 import pandas as pd
 
+REL_FEATURES = ['Stop', 'Station', 'Visibility(mi)', 'Junction', 'Crossing',
+       'Wind_Speed(mph)', 'Pressure(in)', 'Humidity(%)', 'Traffic_Signal',
+       'Temperature(F)', 'Distance(mi)', 'Start_Lat', 'Start_Lng']
 
-df = pd.read_csv('US_Accidents_June20.csv')
-
-flot = ['Start_Lat', 'Start_Lng', 'Distance(mi)', 'Temperature(F)', 'Humidity(%)',
-        'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)']
-bol = ['Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit',
-       'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming',
-       'Traffic_Signal', 'Turning_Loop']
-df = df[flot+bol+['Severity']].dropna()
-print(df.info())
-print(len(df))
-df = df.head(1000000)
-df.loc[df['Severity'].isin([1, 2]), 'Severity'] = 0
-df.loc[df['Severity'].isin([3, 4]), 'Severity'] = 1
+df = pd.read_csv('train.csv')
+df = df[REL_FEATURES]
 
 data_tensor = torch.tensor(df.drop('Severity', axis=1).values.astype('float32'))
 label_tensor = torch.tensor(df['Severity'].values)
