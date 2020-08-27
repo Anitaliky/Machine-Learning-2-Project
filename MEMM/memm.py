61e84e8b8b15f4357751d57970ecdfeeef836e5a
Moshe Abadi <moshe.abadi@campus.technion.ac.il>
2020-08-27 23:49:11 +0300

    continiued memm

--- MEMM/memm.py
+++ MEMM/memm.py
@@ -1,3 +1,4 @@
+import time
 from collections import OrderedDict
 from itertools import combinations
 from numpy.linalg import norm
@@ -10,11 +11,10 @@ import pandas as pd
 
 class Mmem:
 
-    def __init__(self, name, la, threshold, comp=False, k=None):
+    def __init__(self, la, threshold, comp=False, k=None):
         self.likl_func = []
         self.likl_grad = []
-        self.name = name
-        self.tag_set = set()
+        self.tag_set = {0, 1}
         self.histories = []
         self.n_total_features = 0  # Total number of features accumulated
         self.features_list = ['f100', 'f101', 'f102', 'f103', 'f104', 'f105', 'f106', 'f107', 'f108', 'f109', 'f110']
@@ -24,10 +24,7 @@ class Mmem:
         self.B = 7
         self.hyper_str = str(self.threshold) + '_' + str(self.la)
         self.comp = comp
-        if self.comp and self.name == '1':
-            self.model_dir = 'saves/{}_big/'.format(self.name)
-        else:
-            self.model_dir = 'saves/{}/'.format(self.name)
+        self.model_dir = 'saves/'.format()
         if not os.path.exists(self.model_dir) and not self.comp:
             os.makedirs(self.model_dir)
         self.n_dict = {}
@@ -47,42 +44,29 @@ class Mmem:
         else:
             self.features_count[dictionary][key] += 1
 
-    def create_features(self, file_path):
+    def create_features(self):
         """defines and create the features, counting number of occurence of each one."""
-        df = pd.read_csv('train.csv')
+        df = pd.read_csv('/home/student/Desktop/ML/dfs.csv')
         grouped = df.groupby('Airport_Code')
-        for group, df_group in grouped:
-            for i in range(1, 11):
+        for group_id, (group, df_group) in enumerate(grouped):
+            print(group_id, len(df_group))
+            for i in range(10):
                 df_group.loc[-i] = ['*'] * len(df_group.columns)  # adding a row
             df_group.index = df_group.index + 10  # shifting index
-            df_group.sort_index(inplace=True)
-            for i in range(len(df_group)):  # iterate over indices of words in sentence
+            df_group = df_group.sort_index()
+            df_group = df_group.reset_index(drop=True)
+            # print('df_group', df_group.head(12))
+            for i in range(10, len(df_group)):  # iterate over indices of words in sentence
                 history_df = df_group.iloc[i - 10:i + 1].reset_index(drop=True)
                 curr_severity = df_group.loc[i, 'Severity']
                 self.histories.append((history_df, curr_severity))  # add to histories this history and tag
-
-                for size in range(11):
-                    for indx in combinations(range(10), size):
-                        self.add2dict((history_df.loc[indx, 'Severity'], indx, curr_severity), 'f100')
-                        self.add2dict((indx, curr_severity), 'f101')  # value will be distance of temperatures.
-                        self.add2dict((indx, curr_severity), 'f102')  # value will be distance of temperatures.
-
-                # self.add2dict((cur_word.lower(), cur_tag), 'f100')
-                # for suffix_len in range(1, 5):  # [1, 2, 3, 4]
-                #     self.add2dict((cur_word.lower()[-suffix_len:], cur_tag), 'f101')
-                # for prefix_len in range(1, 5):  # [1, 2, 3, 4]
-                #     self.add2dict((cur_word.lower()[:prefix_len], cur_tag), 'f102')
-                # self.add2dict((pre_prev_tag, prev_tag, cur_tag), 'f103')
-                # self.add2dict((prev_tag, cur_tag), 'f104')
-                # self.add2dict((cur_tag,), 'f105')
-                # self.add2dict((prev_word.lower(), cur_tag), 'f106')
-                # next_word = sentence[i + 1] if i + 1 < len(sentence) else 'STOP!'
-                # self.add2dict((next_word.lower(), cur_tag), 'f107')
-                # self.add2dict((pre_prev_word.lower(), prev_word.lower(), cur_tag), 'f108')
-                # self.add2dict((i == 0, cur_word[0].isupper(), cur_tag), 'f109')
-                # cur_word_digit = cur_word.replace('.', '', 1).replace('s', '').replace(r'-', '').replace(r':', '') \
-                #     .replace("'", '').replace(",", '').replace("\/", '').isdigit()
-                # self.add2dict((cur_word_digit, cur_tag), 'f110')
+                # print(i, end=',')
+                for j in range(10):
+                    self.add2dict((history_df.loc[j, 'Severity'], j, curr_severity), 'f100')
+                    self.add2dict((j, curr_severity), 'f101')  # value will be distance of temperatures.
+                    self.add2dict((history_df.loc[j, 'Traffic_Signal'], j, curr_severity), 'f102')  # value will be distance of traffic_signal.
+                    self.add2dict((j, curr_severity), 'f103')  # value will be distance of pressure.
+                    self.add2dict((j, curr_severity), 'f104')  # value will be distance of distance.
 
     def preprocess_features(self):
         """filter features that occured in train set less than threshold,
@@ -95,79 +79,96 @@ class Mmem:
                     self.n_dict[feature_name] += 1
             self.n_total_features = self.n_dict[feature_name]
 
+    @staticmethod
+    def get_distance(history_df, j, col_name):
+        return norm(history_df.loc[j, col_name] - history_df.tail(1)[col_name], 1)
+    # TODO: try 1 -
+
     # history = (sentence_{1:n}(list), T_2, T_1, i)
     def history2features(self, history_df, curr_severity):
         """return a list of features ID given a history"""
         features = {}
         # adds the id of a feature if its conditions happens on the given history and tag
-        for size in range(11):
-            for indx in combinations(range(1, 11), size):
-                features[self.features_id_dict['f100']
-                    [(history_df.loc[indx, 'Severity'].reset_index(drop=True), indx, curr_severity)]] = \
-                    1
-                features[self.features_id_dict['f101'][(indx, curr_severity)]] = \
-                    norm(history_df.loc[indx, 'Temperature(F)'] - history_df.loc.tail(1)['Temperature(F)'], 1)  # TODO: try 1 -
-                features[self.features_id_dict['f102']
-                    [(history_df.loc[indx, 'Traffic_Signal'].reset_index(drop=True), indx, curr_severity)]] = \
-                    1
+        for j in range(10):
+            key = (history_df.loc[j, 'Severity'], j, curr_severity)
+            if key in self.features_id_dict['f100']:
+                features[self.features_id_dict['f100'][key]] = 1
 
-        features += [self.features_id_dict['f100'][(cur_word.lower(), cur_tag)]] \
-            if (cur_word.lower(), cur_tag) in self.features_id_dict['f100'] else []
-        for suffix_len in range(1, 5):  # [1, 2, 3, 4]
-            features += [self.features_id_dict['f101'][(cur_word.lower()[-suffix_len:], cur_tag)]] \
-                if (cur_word.lower()[-suffix_len:], cur_tag) in self.features_id_dict['f101'] else []
-        for prefix_len in range(1, 5):  # [1, 2, 3, 4]
-            features += [self.features_id_dict['f102'][(cur_word.lower()[:prefix_len], cur_tag)]] \
-                if (cur_word.lower()[:prefix_len], cur_tag) in self.features_id_dict['f102'] else []
-        features += [self.features_id_dict['f103'][(pre_prev_tag, prev_tag, cur_tag)]] \
-            if (pre_prev_tag, prev_tag, cur_tag) in self.features_id_dict['f103'] else []
-        features += [self.features_id_dict['f104'][(prev_tag, cur_tag)]] \
-            if (prev_tag, cur_tag) in self.features_id_dict['f104'] else []
-        features += [self.features_id_dict['f105'][(cur_tag,)]] \
-            if (cur_tag,) in self.features_id_dict['f105'] else []
-        features += [self.features_id_dict['f106'][(prev_word.lower(), cur_tag)]] \
-            if (prev_word.lower(), cur_tag) in self.features_id_dict['f106'] else []
-        features += [self.features_id_dict['f107'][(next_word, cur_tag)]] \
-            if (next_word, cur_tag) in self.features_id_dict['f107'] else []
-        features += [self.features_id_dict['f108'][(pre_prev_word.lower(), prev_word.lower(), cur_tag)]] \
-            if (pre_prev_word.lower(), prev_word.lower(), cur_tag) in self.features_id_dict['f108'] else []
-        features += [self.features_id_dict['f109'][(i == 0, cur_word[0].isupper(), cur_tag)]] \
-            if (i == 0, cur_word[0].isupper(), cur_tag) in self.features_id_dict['f109'] else []
-        features += [self.features_id_dict['f110'][(cur_word_digit, cur_tag)]] \
-            if (cur_word_digit, cur_tag) in self.features_id_dict['f110'] else []
+            key = (j, curr_severity)
+            if key in self.features_id_dict['f101']:
+                features[self.features_id_dict['f101'][key]] = self.get_distance(history_df, j, 'Temperature(F)')
+
+            key = (history_df.loc[j, 'Traffic_Signal'], j, curr_severity)
+            if key in self.features_id_dict['f102']:
+                features[self.features_id_dict['f102'][key]] = 1
+
+            key = (j, curr_severity)
+            if key in self.features_id_dict['f103']:
+                features[self.features_id_dict['f103'][key]] = self.get_distance(history_df, j, 'Pressure(in)')
+
+            key = (j, curr_severity)
+            if key in self.features_id_dict['f104']:
+                features[self.features_id_dict['f104'][key]] = self.get_distance(history_df, j, 'Distance(mi)')
+
+
+
+        # features += [self.features_id_dict['f100'][(cur_word.lower(), cur_tag)]] \
+        #     if (cur_word.lower(), cur_tag) in self.features_id_dict['f100'] else []
+        # for suffix_len in range(1, 5):  # [1, 2, 3, 4]
+        #     features += [self.features_id_dict['f101'][(cur_word.lower()[-suffix_len:], cur_tag)]] \
+        #         if (cur_word.lower()[-suffix_len:], cur_tag) in self.features_id_dict['f101'] else []
+        # for prefix_len in range(1, 5):  # [1, 2, 3, 4]
+        #     features += [self.features_id_dict['f102'][(cur_word.lower()[:prefix_len], cur_tag)]] \
+        #         if (cur_word.lower()[:prefix_len], cur_tag) in self.features_id_dict['f102'] else []
+        # features += [self.features_id_dict['f103'][(pre_prev_tag, prev_tag, cur_tag)]] \
+        #     if (pre_prev_tag, prev_tag, cur_tag) in self.features_id_dict['f103'] else []
+        # features += [self.features_id_dict['f104'][(prev_tag, cur_tag)]] \
+        #     if (prev_tag, cur_tag) in self.features_id_dict['f104'] else []
+        # features += [self.features_id_dict['f105'][(cur_tag,)]] \
+        #     if (cur_tag,) in self.features_id_dict['f105'] else []
+        # features += [self.features_id_dict['f106'][(prev_word.lower(), cur_tag)]] \
+        #     if (prev_word.lower(), cur_tag) in self.features_id_dict['f106'] else []
+        # features += [self.features_id_dict['f107'][(next_word, cur_tag)]] \
+        #     if (next_word, cur_tag) in self.features_id_dict['f107'] else []
+        # features += [self.features_id_dict['f108'][(pre_prev_word.lower(), prev_word.lower(), cur_tag)]] \
+        #     if (pre_prev_word.lower(), prev_word.lower(), cur_tag) in self.features_id_dict['f108'] else []
+        # features += [self.features_id_dict['f109'][(i == 0, cur_word[0].isupper(), cur_tag)]] \
+        #     if (i == 0, cur_word[0].isupper(), cur_tag) in self.features_id_dict['f109'] else []
+        # features += [self.features_id_dict['f110'][(cur_word_digit, cur_tag)]] \
+        #     if (cur_word_digit, cur_tag) in self.features_id_dict['f110'] else []
 
         return features
 
-    def linear_term(self, w):
-        """calculate the linear term of the likelihood function: sum for each i: w*f(x_i,y_i)"""
-        linear_sum = 0
-        for feature in self.train_features.values():
-            linear_sum += w[feature].sum()
-        return linear_sum
 
     def extract_features(self):
-        """calculate once: sum for each i and for each y': f(x_i, y')"""
         for history, cur_tag in self.histories:
             self.train_features[(history, cur_tag)] = self.history2features(history, cur_tag)
             for y_ in self.tag_set:
                 self.all_tags_features[(history, y_)] = self.history2features(history, y_)
 
-    def normalization_term(self, w):
+
+    def linear_term(self, w):
         """calculate the linear term of the likelihood function: sum for each i: w*f(x_i,y_i)"""
+        linear_sum = 0
+        for feature_dict in self.train_features.values():
+            linear_sum += (w[list(feature_dict.keys())]*list(feature_dict.values())).sum()
+        return linear_sum
+
+    def normalization_term(self, w):
         all_sum = 0
         for history, _ in self.histories:
             log_sum = 0
             for y_ in self.tag_set:
-                log_sum += np.exp(w[self.all_tags_features[(history, y_)]].sum())
+                feature_dict = self.all_tags_features[(history, y_)]
+                log_sum += np.exp((w[list(feature_dict.keys())] * list(feature_dict.values())).sum())
             self.all_tags_exp[history] = log_sum
             all_sum += np.log(log_sum)
         return all_sum
 
     def empirical_counts(self):
-        """calculate the linear term of the likelihood function: sum for each i: w*f(x_i,y_i)"""
         self.train_vector_sum = np.zeros(self.n_total_features)
-        for feature in self.train_features.values():
-            self.train_vector_sum[feature] += 1
+        for feature_dict in self.train_features.values():
+            self.train_vector_sum[list(feature_dict.keys())] += list(feature_dict.values())
 
     def expected_counts(self, w):
         """calculate the expected term of the loss function"""
@@ -175,8 +176,9 @@ class Mmem:
         for history, cur_tag in self.histories:
             deno = self.all_tags_exp[history]
             for y_ in self.tag_set:
-                nom = np.exp(w[self.all_tags_features[(history, y_)]].sum())
-                all_sum[self.all_tags_features[(history, y_)]] += (nom / deno)
+                feature_dict = self.all_tags_features[(history, y_)]
+                nom = np.exp((w[list(feature_dict.keys())] * list(feature_dict.values())).sum())
+                all_sum[list(self.all_tags_features[(history, y_)].values())] += (nom / deno)
         return all_sum
 
     def target_funcs(self, w):
@@ -201,18 +203,14 @@ class Mmem:
         """saves the train result to be able to test using them"""
         with open(self.model_dir + 'weights.pkl', 'wb') as file:
             pickle.dump(self.w, file)
-        with open(self.model_dir + 'tag_set.pkl', 'wb') as file:
-            pickle.dump(self.tag_set, file)
         with open(self.model_dir + 'features_id_dict.pkl', 'wb') as file:
             pickle.dump(self.features_id_dict, file)
 
     def train(self, train_path=None):
         """train the model"""
-        if train_path is None:
-            train_path = 'data/train{}.wtag'.format(self.name)
-        print("Beginning train on train{}.wtag".format(self.name))
+        print('Beginning train')
 
-        self.create_features(train_path)
+        self.create_features()
         self.preprocess_features()
         self.extract_features()
         self.minimize()
@@ -221,26 +219,12 @@ class Mmem:
     def test(self, m=None, test_path=None, run_train=True):
         """test the model"""
         if test_path is None:
-            if self.comp:
-                print("Starting to tag competition", self.name)
-                test_path = 'data/comp{}.words'.format(self.name)
-            else:
-                if self.name == '1':
-                    print("Starting test on test1.wtag")
-                    test_path = 'data/test{}.wtag'.format(self.name)
-                elif self.name == '2':
-                    print("Starting test on train2.wtag")
-                    test_path = 'data/train{}.wtag'.format(self.name)
-        if m is None:
-            if self.comp or self.name == '1':
-                m = 1000
-            else:
-                m = 250
+            test_path = 'data/test.csv'
+        print("Beginning test on", test_path)
+
         if not run_train:
             with open(self.model_dir + 'weights.pkl', 'rb') as file:
                 self.w = pickle.load(file)
-            with open(self.model_dir + 'tag_set.pkl', 'rb') as file:
-                self.tag_set = pickle.load(file)
             with open(self.model_dir + 'features_id_dict.pkl', 'rb') as file:
                 self.features_id_dict = pickle.load(file)
 
@@ -409,27 +393,19 @@ class Mmem:
 
 
 if __name__ == '__main__':
-    print('Welcome to our superPOStagger!')
-    while True:
-        name = input("Which model should we run?[1/2]  ")
-        if name.lower() in ['1', '2']:
-            break
-        print("Invalid input. Please enter '1' or '2'")
-    while True:
-        run_train = input("Should we train before testing?[y/n]   ")
-        if run_train.lower() in ['y', 'n', 'yes', 'no']:
-            run_train = True if run_train in ['y', 'yes'] else False
-            break
-        print("invalid input. please enter 'y' or 'n'")
+    print('Welcome to our accidents predictor!')
+    # while True:
+    #     run_train = input("Should we train before testing?[y/n]   ")
+    #     if run_train.lower() in ['y', 'n', 'yes', 'no']:
+    #         run_train = True if run_train in ['y', 'yes'] else False
+    #         break
+    #     print("invalid input. please enter 'y' or 'n'")
 
-    if name == '1':
-        thre = 3
-        la = 5
-    elif name == '2':
-        thre = 1
-        la = 0.1
+    run_train = True
+    thre = 1
+    la = 0.1
 
-    model = Mmem(name, la=la, threshold=thre)
+    model = Mmem(la=la, threshold=thre)
     if run_train:
         model.train()
     model.test(run_train=run_train)
