8bf1f06ea74e8b3346c2ea0006632607dddb3b8a
Anitaliky <anitaliky@gmail.com>
2020-09-06 16:32:30 +0300

    Final CSV files

--- SVM/svm.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sklearn
-import numpy as np
-
-
-if __name__ == '__main__':
-    df = pd.read_csv("/Users/MosheAbadi/Downloads/US_Accidents_June20.csv")
-    print(df.columns)
-
